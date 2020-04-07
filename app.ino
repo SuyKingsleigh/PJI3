7150352cc498cd9a26a147079237ff23fa8cd870
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2020-04-06 21:16:01 -0300

    instanciacao dos metodos do lora e bluetooth

--- app.ino
+++ app.ino
@@ -8,7 +8,8 @@
 #define SENSOR_TEMP_PIN 34 //analog adc1_ch6
 #define SD_CS 5 // pino do cartao S
 
-#define MAX_ATTEMPTS 5 // maximo de tentativas pra enviar uam mensagem pelo LORA
+#define MAX_ATTEMPTS_LORA 5 // maximo de tentativas pra enviar uam mensagem pelo LORA
+#define MAX_ATTEMPTS_BLE 500 // maximo de tentativas pra enviar uam mensagem pelo BLE
 #define TOUT 500 // tempo de timeout ms
 
 
@@ -26,11 +27,14 @@ void setup(){
     // tentara enviar pelo bluetooth
     // para isso ele hibernara por 30 segundos 
     // e entao tentara enviar pelo bluetooth N vezes
-    if(!send_msg_lora(msg, MAX_ATTEMPTS, TOUT))
-        while(!hibernate_and_exec_func(&send_msg_bluetooth, msg, 30, MAX_ATTEMPTS)) {;}
+    if(!send_msg_lora(msg, MAX_ATTEMPTS_LORA, TOUT))
+        while(!hibernate_and_exec_func(&send_msg_bluetooth, msg, 30, MAX_ATTEMPTS_BLE)) {;}
 
     // depois de enviar pelo lora ou bluetooth, hiberna por meia hora 
+    // hibernar depois de tentar enviar pelo bluetooth eh necessario para poupar bateria
+    // uma vez que o tentar enviar pelo bluetooth consumira muito mais do que pelo LORA
     hibernate();
 }   
 
+// nunca chega no loop
 void loop(){}
